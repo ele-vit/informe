971fb4053e5d84767a84b71ae11a4bbaed03dd66
Elevit <elevit.pachon@squadmakers.com>
2023-05-24 20:29:48 -0500

    report

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-apt-get install add py3-pip py3-pillow py3-cffi py3-brotli gcc musl-dev python3-dev pango
+apt-get install -y py3-pip py3-pillow py3-cffi py3-brotli gcc musl-dev python3-dev pango
 python manage.py collectstatic --no-input
 python manage.py migrate
