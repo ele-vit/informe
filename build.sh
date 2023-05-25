3257b817dc94049a86f9440bff65961ff9f247eb
Elevit <elevit.pachon@squadmakers.com>
2023-05-24 19:29:05 -0500

    error libpango

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-sudo apt-get update
-sudo apt-get upgrade libpango-1.0-0
+apt update
+apt upgrade libpango-1.0-0
 
 python manage.py collectstatic --no-input
 python manage.py migrate
